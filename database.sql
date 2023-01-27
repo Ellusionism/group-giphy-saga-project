19b9992974aee37e9e1c024bfe6cb072f4e4bb3f
Bishar00 <bishar1yusuf@gmail.com>
2023-01-27 10:43:55 -0600

    likelist

--- database.sql
+++ database.sql
@@ -12,3 +12,15 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+CREATE TABLE "likes" (
+	"id" SERIAL PRIMARY KEY,
+	"url" VARCHAR,
+	"title" VARCHAR,
+	"category_id" INT REFERENCES "category" 
+	
+);
+
+
+INSERT INTO "likes" ("url", "title") 
+VALUES ('https://media2.giphy.com/media/IsiP1Q7ANM8Q10HHKU/200.gif?cid=a4aca14didgm9rs4d08frlsulbzlfjnv4a56xbtcz0u0uhk0&rid=200.gif&ct=g','Travel Beach GIF by Rome & Duddy');
